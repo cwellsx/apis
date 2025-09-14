6d89180e8ac3ee539c78684a42cb54e87e47ad78
cwellsx <cwellsx@gmail.com>
2025-09-14 08:32:08 +0200

    .vscode files to launch vscode-ext

--- packages/vscode-ext/src.webview/compile.cmd
+++ packages/vscode-ext/src.webview/compile.cmd
@@ -1,5 +1,5 @@
 @echo off
 REM Compile a TypeScript file to JavaScript
-npx tsc --outDir ../media --project tsconfig.json
+npx tsc --outDir "%~dp0..\media" --project "%~dp0tsconfig.webview.json"
 echo Compilation complete.
 pause
