0f9a372f97ec89c097c4b49c6ed21ab6019a1fbc
cwellsx <cwellsx@gmail.com>
2025-10-19 19:20:38 +0200

    Automatically build prerequisites

--- packages/vscode-ext/src.webview/compile.cmd
+++ packages/vscode-ext/src.webview/compile.cmd
@@ -1,5 +1,5 @@
 @echo off
 REM Compile a TypeScript file to JavaScript
-npx tsc --outDir "%~dp0..\media" --project "%~dp0tsconfig.webview.json"
+npx tsc -build "%~dp0tsconfig.webview.json"
 echo Compilation complete.
 pause
