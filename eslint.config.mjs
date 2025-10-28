254c0e465d49fbcf9676772cb15215e5354d884e
cwellsx <cwellsx@gmail.com>
2025-10-28 07:13:17 +0100

    TypeScript-enabled linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,21 @@ import eslint from "@eslint/js";
 import { defineConfig } from "eslint/config";
 import tseslint from "typescript-eslint";
 
-const ignorePaths = ["packages/electron-app/src/renderer/3rd-party/**"];
+const ignorePaths = [
+  // per-file override: do not use project-based type-checking for the config file itself
+  "eslint.config.mjs",
+  // don't lint 3rd-party source code
+  "packages/electron-app/src/renderer/3rd-party/**",
+];
 
 export default defineConfig([
   {
     ignores: ignorePaths,
-    extends: [eslint.configs.recommended, tseslint.configs.recommended],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+      },
+    },
+    extends: [eslint.configs.recommended, tseslint.configs.recommendedTypeChecked],
   },
 ]);
